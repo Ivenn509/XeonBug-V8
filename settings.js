ed4a9311d148e8bd317e99e178d511e30956ed37
Ivenn509 <botpanealdeploy@gmail.com>
2024-10-19 09:59:02 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "50937664555"
+global.ownername = "yann"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '50937664555'  //creator number
+global.ownername = 'yann' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
